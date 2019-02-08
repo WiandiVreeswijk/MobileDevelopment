2b577cd772aa901253fda971d94725b1621974a7
Wiandi Vreeswijk <wiavre@gmail.com>
2019-02-08 12:49:51 +0100

    Added constraints

--- HigherLower/app/src/main/java/com/example/higherlower/MainActivity.java
+++ HigherLower/app/src/main/java/com/example/higherlower/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.higherlower;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private int score = 0;
+    private int highScore = 0;
 
+    private int valueCurrent = 0;
+    private int valuePrevious = 0;
+
+    private TextView mScore;
+    private TextView mHighScore;
+    private TextView mLastThrow;
+    private ImageView mDice;
+    private Button mLower;
+    private Button mRestart;
+    private Button mHigher;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        score = findViewById(R.id.textViewScore);
     }
 }
