e6de9f09e8b88383ae224add2bb50da073c9b313
Wiandi Vreeswijk <wiavre@gmail.com>
2019-02-08 13:07:06 +0100

    Initialized variables + added random dice throw method

--- HigherLower/app/src/main/java/com/example/higherlower/MainActivity.java
+++ HigherLower/app/src/main/java/com/example/higherlower/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.higherlower;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     private int score = 0;
@@ -25,6 +28,56 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        score = findViewById(R.id.textViewScore);
+        mScore = findViewById(R.id.textViewScore);
+        mHighScore = findViewById(R.id.textViewHighScore);
+        mLastThrow = findViewById(R.id.textViewLastThrow);
+        mDice = findViewById(R.id.imageView2);
+        mLower = findViewById(R.id.buttonLower);
+        mHigher = findViewById(R.id.buttonHigher);
+        mRestart = findViewById(R.id.buttonReset);
+
+        mLower.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rollDice();
+            }
+        });
+        mHigher.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rollDice();
+            }
+        });
+        mRestart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+    private void rollDice() {
+        valuePrevious = valueCurrent;
+        valueCurrent = new Random().nextInt(6) + 1;
+
+        switch (valueCurrent) {
+            case 1:
+                mDice.setImageDrawable(getDrawable(R.drawable.dice1));
+                break;
+            case 2:
+                mDice.setImageDrawable(getDrawable(R.drawable.dice2));
+                break;
+            case 3:
+                mDice.setImageDrawable(getDrawable(R.drawable.dice3));
+                break;
+            case 4:
+                mDice.setImageDrawable(getDrawable(R.drawable.dice4));
+                break;
+            case 5:
+                mDice.setImageDrawable(getDrawable(R.drawable.dice5));
+                break;
+            case 6:
+                mDice.setImageDrawable(getDrawable(R.drawable.dice6));
+                break;
+        }
     }
 }
